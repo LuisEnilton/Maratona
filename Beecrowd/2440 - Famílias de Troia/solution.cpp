8aaeb814a3228e1e22616298c17c4a8e6533283e
Luis Enilton <110181744+LuisEnilton@users.noreply.github.com>
2023-07-14 13:40:03 -0300

    Configuration

--- Beecrowd/2440 - Famílias de Troia/solution.cpp
+++ Beecrowd/2440 - Famílias de Troia/solution.cpp
@@ -5,16 +5,19 @@
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 
-#define optimize ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define optimize                 \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL);               \
+    cout.tie(NULL)
 #define INF 1000000010
 #define INFLL 1000000000000000010LL
 #define ALL(x) x.begin(), x.end()
 #define UNIQUE(c) (c).resize(unique(ALL(c)) - (c).begin())
-#define REP(i, a, b) for(int i = (a); i <= (b); i++)
+#define REP(i, a, b) for (int i = (a); i <= (b); i++)
 #define POS(n, x) (lower_bound(ALL(n), x) - (n).begin())
 #define ll long long
 #define ld long double
-#define pii pair<int,int>
+#define pii pair<int, int>
 #define vi vector<int>
 #define vii vector<pii>
 #define os_type int
@@ -30,54 +33,58 @@
 using namespace std;
 using namespace __gnu_pbds;
 
-#define ordered_set tree<os_type, null_type,less<os_type>, rb_tree_tag,tree_order_statistics_node_update>
+#define ordered_set tree<os_type, null_type, less<os_type>, rb_tree_tag, tree_order_statistics_node_update>
 
 int N, M;
 
 int pai[MAXN];
 int tamanho[MAXN];
 
-
-int find(int u) {
-    if (u == pai[u]) return u;
+int find(int u)
+{
+    if (u == pai[u])
+        return u;
     return pai[u] = find(pai[u]);
-    //dp para não pesquisar o pai 2 vezes
-
+    // dp para não pesquisar o pai 2 vezes
 }
 
-void join(int u, int v) {
+void join(int u, int v)
+{
     u = find(u);
     v = find(v);
-    if(u == v)  return;
+    if (u == v)
+        return;
 
-    if(tamanho[u] > tamanho[v])
-        swap(u,v);
+    if (tamanho[u] > tamanho[v])
+        swap(u, v);
 
     pai[u] = v;
 
     tamanho[v] += tamanho[u];
 }
 
-
-int main() {
+int main()
+{
     optimize;
     cin >> N >> M;
-    for (int i = 1; i <= N ; i++) {
+    for (int i = 1; i <= N; i++)
+    {
         pai[i] = i;
         tamanho[i] = 1;
     }
 
-    for (int i = 0; i < M; i++) {
+    for (int i = 0; i < M; i++)
+    {
         int u, v;
         cin >> u >> v;
-        join(u,v);
+        join(u, v);
     }
     set<int> pais;
-    int cont = 0 ;
-    for (int i = N; i > 0; i--) {
-        int pai2 = find( i);
-            pais.insert(pai2);
-
-        }
-    cout<<pais.size()<<endl;
+    int cont = 0;
+    for (int i = N; i > 0; i--)
+    {
+        int pai2 = find(i);
+        pais.insert(pai2);
     }
+    cout << pais.size() << endl;
+}
--- teste.cpp
+++ teste.cpp
@@ -6,12 +6,9 @@ using namespace std;
 
 
 
-
-
 int main(int argc, char const *argv[])
 {
-    Solution sol = *new Solution();
-    vi nums{2,7,9,3,1};
-    cout <<sol.rob(nums) << endl;
+    
+    cout << "hello world" << endl;
     return 0;
 }
