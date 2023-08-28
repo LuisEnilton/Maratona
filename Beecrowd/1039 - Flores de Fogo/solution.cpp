4082f20b78d270fae347fe9e2b001c478a1fc568
Luis Enilton <110181744+LuisEnilton@users.noreply.github.com>
2023-08-28 16:13:32 -0300

    1039

---
+++ Beecrowd/1039 - Flores de Fogo/solution.cpp
@@ -0,0 +1,57 @@
+//
+// Created by Luis on 28/08/2023.
+//
+//Template By eduardocesb
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+#define optimize ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define INF 1000000010
+#define INFLL 1000000000000000010LL
+#define ALL(x) x.begin(), x.end()
+#define UNIQUE(c) (c).resize(unique(ALL(c)) - (c).begin())
+#define REP(i, a, b) for(int i = (a); i <= (b); i++)
+#define POS(n, x) (lower_bound(ALL(n), x) - (n).begin())
+#define ll long long
+#define ld long double
+#define pii pair<int,int>
+#define vi vector<int>
+#define vii vector<pii>
+#define os_type int
+#define PB push_back
+#define EB emplace_back
+#define MOD 1000000007
+#define PRIME 101
+#define MAXN 1010101
+#define MAXL 23
+#define EPS 1e-9
+#define endl '\n'
+#define x first
+#define y second
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ordered_set tree<os_type, null_type,less<os_type>, rb_tree_tag,tree_order_statistics_node_update>
+typedef pair<int, int> Point;
+
+double distance(Point a, Point b) {
+    return sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
+}
+const double pi = acos(-1);
+
+int main(int argc, char **argv) {
+    //optimize;
+    Point c1, c2;
+    int r1, r2;
+    while (cin >> r1 >> c1.x >> c1.y >> r2 >> c2.x >> c2.y) {
+        double dist = distance(c1, c2);
+        if (dist + double(r2) - double(r1) > EPS) {
+            cout << "MORTO" << endl;
+        } else {
+            cout << "RICO" << endl;
+        }
+    }
+    return 0;
+}
+
