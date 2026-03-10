888c6feb43c5b349f61fb3735d5cee63a1eee996
Luis Enilton <luiseniltonxy@gmail.com>
2026-03-10 11:30:59 -0300

    update Sparse table

--- Biblioteca/DS/SparseTable.cpp
+++ Biblioteca/DS/SparseTable.cpp
@@ -5,60 +5,57 @@ using namespace std;
 #define MAXL 23
 #define int long long
 #define vi vector<int>
-int m[MAXN][MAXL];
+#define INF 1000000010
 
-// Funciona pra Min, Max e GCD em 0(1), idempotente
-// O(log) para associativas não idempotentes
+struct SparseTable
+{
 
-void precalc(vi &a) {
-    int n = a.size();
-    // O primeiro nivel são os numeros lidos no input
-    for (int i = 0; i < n; i++)
-        m[i][0] = a[i];
+    vector<vi> m;
+    int n, maxl;
+    SparseTable(vi &a)
+    {
+        n = a.size();
+        maxl = log2(a.size()) + 1;
+        m.resize(n, vi(maxl, -INF));
+        precalc(a);
+    }
 
-    for (int k = 1; k < MAXL; k++) {
-        for (int i = 0; i + (1 << k) - 1 < n; i++) {
-            m[i][k] = (m[i][k - 1] + m[i + (1 << (k - 1))][k - 1]);
+    void precalc(vi &a)
+    {
+        for (int i = 0; i < n; i++)
+            m[i][0] = a[i];
+        for (int k = 1; k < maxl; k++)
+        {
+            for (int i = 0; i + (1 << k) - 1 < n; i++)
+            {
+                m[i][k] = max(m[i][k - 1], m[i + (1 << (k - 1))][k - 1]);
+            }
         }
     }
-}
+    int query(int l, int r)
+    {
+        // RODAR O PRECALC
+        int j = __builtin_clz(1) - __builtin_clz(r - l + 1);
 
-signed query(int l, int r) {
-    // RODAR O PRECALC
-    int j = __builtin_clz(1) - __builtin_clz(r - l + 1);
-
-    return min(m[l][j], m[r - (1 << j) + 1][j]);
-}
-
-// O(log)
-int sum_query(int l, int r) {
-    // 0-indexed
-    int ans = 0;
-    for (int j = MAXL - 1; j >= 0; j--) {
-        if (l + (1 << j) - 1 <= r) {
-            ans = ans + m[l][j];
-
-            // instead of having L', we
-            // increment L directly
-            l += 1 << j;
-        }
+        return max(m[l][j], m[r - (1 << j) + 1][j]);
     }
-    return ans;
-}
+};
 
-signed main() {
+signed main()
+{
     int n;
     cin >> n;
     vi nums(n);
-    for (auto &x: nums)
+    for (auto &x : nums)
         cin >> x;
-    precalc(nums);
+    auto st = SparseTable(nums);
     int q;
     cin >> q;
-    while (q--) {
+    while (q--)
+    {
         int l, r;
         cin >> l >> r;
-        cout << sum_query(l, r) << endl;
+        cout << st.query(l, r) << endl;
     }
 
     return 0;
