23c4934af65dbcbcfa423c7cab46f3c7bbd30751
Luis Enilton <luiseniltonxy@gmail.com>
2026-03-19 15:25:10 -0300

    Add seg crivo

--- Biblioteca/Matemática/Crivo.cpp
+++ Biblioteca/Matemática/Crivo.cpp
@@ -57,6 +57,17 @@ vector<int> crivo(ll N)
     return eh_primo;
 }
 
+vector<bool> segCrivo(long long L, long long R) {
+    vector<bool> isPrime(R - L + 1, true);
+    long long lim = sqrt(R);
+    for (long long i = 2; i <= lim; ++i)
+        for (long long j = max(i * i, (L + i - 1) / i * i); j <= R; j += i)
+            isPrime[j - L] = false;
+    if (L == 1)
+        isPrime[0] = false;
+    return isPrime;
+}
+
 
 
 int main() {
